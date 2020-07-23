e7962dbfcdf4459a0d34ef20478769b7cd61373d
rakibsta <crakib123@gmail.com>
2020-07-23 18:34:02 -0400

    no change

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -31,9 +31,9 @@ bool RemoteControlCodeEnabled = true;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
- * 
+ *
  * This should be called at the start of your int main function.
  */
-void vexcodeInit( void ) {
+void vexcodeInit( void ){
   // nothing to initialize
 }
