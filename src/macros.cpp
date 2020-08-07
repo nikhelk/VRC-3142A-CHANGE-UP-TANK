7c5d40b9896eb4dccfc18b926409af757feb4b30
Nikhel Krishna <krishna.nikhel@paideiaschool.org>
2020-08-07 15:45:03 -0400

    manually merged macros

--- src/macros.cpp
+++ src/macros.cpp
@@ -1,7 +1,6 @@
-
 #include "vex.h"
-visionSensor ejector = visionSensor::BLACK;
 botStates myBot;
+visionSensor ejector = visionSensor::BLACK;
 bool getS1 ();
 bool getS1Status ()
 {
@@ -9,7 +8,7 @@ bool getS1Status ()
     {
       return true;
     }
-  else 
+  else
   {
     return false;
   }
@@ -21,36 +20,114 @@ bool getS2Status()
   {
     return true;
   }
-  else 
+  else
   {
     return false;
   }
 }
 
 
-void setMacroState() {
+void setMacroState()
+{
+  if(getS2Status())
+  {
+    if(getIsShooting())
+    {
+      myBot = S2_BALL_SHOOTING;
+    }
+    else
+    {
+      if(IntakeL.power()>8)
+      {
+        myBot = S2_BALL_N_SHOOTING_I_HIGH;
 
+      }
+      else
+      {
+        myBot = S2_BALL_N_SHOOTING_I_LOW;
+      }
+    }
 
+  }
+  else
+  {
+    //Vision.takeSnapshot(SIG_1);
+    if (Vision.objects[0].exists)
+    {
+      if (IntakeL.power() > 8)
+      {
+        myBot = S2_EMPTY_E_SAME_I_HIGH;
+      }
+      else
+      {
+        myBot = S2_EMPTY_E_SAME_I_LOW;
+      }
+    }
+    else
+    {
+     // Vision.takeSnapshot(SIG_2);
+      if (Vision.objects[0].exists)
+      {
+        myBot = S2_EMPTY_E_OPP;
+      }
+      else
+      {
+        if(getS1())
+        {
+          if(IntakeL.power()>8)
+          {
+            myBot = S2_EMPTY_E_EMPTY_S1_BALL_I_HIGH;
+          }
+          else
+          {
+            myBot = S2_EMPTY_E_EMPTY_S1_BALL_I_LOW;
+          }
+        }
+
+        else
+        {
+          if(IntakeL.power()>8)
+          {
+            myBot = S2_EMPTY_E_EMPTY_S1_EMPTY_I_HIGH;
+          }
+          else
+          {
+            myBot = S2_EMPTY_E_EMPTY_S1_EMPTY_I_LOW;
+          }
+        }
+      }
+    }
+  }
 }
-
-
+//bruhhhhh
 bool getIsShooting()
 {
   return (Controller1.ButtonL1.pressing());
 }
 
-void indexerUser()
+void macrosUser()
 {
   switch (myBot)
   {
+    case S2_EMPTY_E_SAME_I_HIGH:
+      break;
+    case S2_EMPTY_E_SAME_I_LOW:
+      break;
+    case S2_EMPTY_E_OPP:
+      break;
+    case S2_EMPTY_E_EMPTY_S1_EMPTY_I_HIGH:
+      break;
+    case S2_EMPTY_E_EMPTY_S1_EMPTY_I_LOW:
+      break;
+    case S2_EMPTY_E_EMPTY_S1_BALL_I_HIGH:
+      break;
+    case S2_EMPTY_E_EMPTY_S1_BALL_I_LOW:
+      break;
+    case S2_BALL_SHOOTING:
+      break;
+    case S2_BALL_N_SHOOTING_I_HIGH:
+      break;
+    case S2_BALL_N_SHOOTING_I_LOW:
+      break;
   }
-}
-
-void flywheelUser()
-{
-
-}
-void intakeUser()
-{
- 
 }
