1f778d04134073305636cc6fa5d37e502b651c41
moleaabu1 <64035363+moleaabu1@users.noreply.github.com>
2020-08-21 19:30:53 -0400

    Update posPID.h

--- include/ChassisSystems/posPID.h
+++ include/ChassisSystems/posPID.h
@@ -36,25 +36,26 @@ public:
    * @param kP desired kP value
    * @param kD desired kD value
    */
-
-  posPID(double kP, double kD);
-
-  /**
-   * Calculates power of PD drive controller definied in FourMotorDrive
-   * @param targetPos  desired position of robot
-   * @param currentPos current position of robot
-   * @return output power of controller
-   */
-
+  
   void setPD(double kP, double kD);
-
+  
+  
   /**
    * Creates a PD controller for drivetrain
    * @param kP value
    * @param kD value
    */
 
+  posPID(double kP, double kD);
 
+
+
+/**
+   * Calculates power of PD drive controller
+   * @param targetPos  desired position of robot
+   * @param currentPos current position of robot
+   * @return output power of controller
+   */
 
   double calculatePower(double targetPos, double currentPos);
 
@@ -65,8 +66,10 @@ public:
 
   double getError() { return (m_error); }
 
+  //gets kP value of controller
   double getKp() {return (m_kP);}
 
+  //gets kD value of controller 
   double getKd() {return(m_kD);}
 
   /**
