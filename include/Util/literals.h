04e41ba7d9ce3d7efc2a7e469beaaf8b7a0ac432
Nikhel <krishna.nikhel@paideiaschool.org>
2020-09-04 00:00:55 -0400

    deleted literals.cpp and moved all to literals.h

--- include/Util/literals.h
+++ include/Util/literals.h
@@ -1,19 +1,19 @@
 #include "Util/vex.h"
 
 /// inches operator (coverted to meters)
-long double operator"" _in(long double x);
+long double operator"" _in(long double x) {return(x*0.0254);}
 
 /// meters operator
-long double operator"" _m(long double x);
+long double operator"" _m(long double x) {return(x);};
 
 /// radians operator
-long double operator"" _rad(long double x);
+long double operator"" _rad(long double x) {return(x);};
 
 /// degree operator (converted to radians)
-long double operator"" _deg(long double x);
+long double operator"" _deg(long double x) {return (x * (M_PI / 180));}
 
 /// meters per second operator 
-long double operator"" _mps(long double x);
+long double operator"" _mps(long double x) {return(x);};
 
 /// meters per second^2 operator
-long double operator"" _mps2(long double x);
+long double operator"" _mps2(long double x) {return(x);};
