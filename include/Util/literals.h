64cedfafe71a34bf8cea9c3b5a0a98f8da34dc29
Nikhel <krishna.nikhel@paideiaschool.org>
2020-09-04 00:27:03 -0400

    re-added Util source files

--- include/Util/literals.h
+++ include/Util/literals.h
@@ -1,19 +1,19 @@
 #include "Util/vex.h"
 
 /// inches operator (coverted to meters)
-long double operator"" _in(long double x) {return(x*0.0254);}
+long double operator"" _in(long double x);
 
 /// meters operator
-long double operator"" _m(long double x) {return(x);};
+long double operator"" _m(long double x);
 
 /// radians operator
-long double operator"" _rad(long double x) {return(x);};
+long double operator"" _rad(long double x);
 
 /// degree operator (converted to radians)
-long double operator"" _deg(long double x) {return (x * (M_PI / 180));}
+long double operator"" _deg(long double x);
 
 /// meters per second operator 
-long double operator"" _mps(long double x) {return(x);};
+long double operator"" _mps(long double x);
 
 /// meters per second^2 operator
-long double operator"" _mps2(long double x) {return(x);};
+long double operator"" _mps2(long double x);
