318328da837006819c264b0b323383a2fc0c30ee
Nikhel <krishna.nikhel@paideiaschool.org>
2020-09-15 13:20:24 -0400

    removed globalBools struct

--- include/Impl/auto_skills.h
+++ include/Impl/auto_skills.h
@@ -7,31 +7,3 @@ void runAutoSkills();
 void testAutoSkills();
 
 extern bool atGoal;
-
-struct globalBools {
-  static bool FlywheelStopWhenTopDetected;
-  static bool scored;
- static bool backUp;
- static bool IntakesRunContinously;
-static  bool IntakesStop;
-static  bool IndexerStopWhenTopDetected;
-static  bool IndexerStopWhenMiddleDetected;
-static  bool IndexerStopWhenBottomDetected;
- static bool IndexerRunContinuously;
- static bool IndexerStop;
-
-  void resetBools() {
-    FlywheelStopWhenTopDetected =
-    scored =
-    backUp =
-    IntakesRunContinously =
-    IndexerStopWhenTopDetected =
-    IndexerStopWhenMiddleDetected =
-    IndexerStopWhenBottomDetected =
-    IndexerRunContinuously =
-    IntakesStop =
-    IndexerStop =
-    false;
-  }
-
-};
